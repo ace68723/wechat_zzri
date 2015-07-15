d8b2533b5ca364296244bc7256f5e911d701278f
ace68723 <ace68723@gmail.com>
2015-07-15 14:58:52 -0400

    zzrs

---
+++ doc/rcmd_api_ex.js
@@ -0,0 +1,45 @@
+{
+	"recommend_id":{
+		"recommend_name":"午餐精选",
+		"cat_id":{
+			cat_name:"必食介绍",
+			cat_list:[{	dish_id:"12312312asdasd"
+
+						},{
+							dish_id:"12312312asdasd"	
+						},{
+							dish_id:"12312312asdasd"	
+					}]
+		}					
+	},
+	"recommend_id":{
+		"recommend_name":"蒸字招牌",
+		"cat_id":{
+			cat_name:"海鲜篇",
+			cat_list:[{	dish_id:"12312312asdasd"
+
+						},{
+							dish_id:"12312312asdasd"	
+						},{
+							dish_id:"12312312asdasd"	
+					}]
+		},
+		"cat_id":{
+			cat_name:"海鲜篇",
+			cat_list:[{	dish_id:"12312312asdasd"
+
+						},{
+							dish_id:"12312312asdasd"	
+						},{
+							dish_id:"12312312asdasd"	
+					}]
+		}					
+	}
+}
+
+dish:
+"dish_id":{
+	 "dish_name":"超值。。。。",
+	 "date": " "
+	 "content":" html "
+}
